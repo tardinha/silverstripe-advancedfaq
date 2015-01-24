e776f8211ff5499f778bffb50c6cb1cb6c02f332
Mark Taylor <tardinha@gmail.com>
2015-01-24 16:09:16 +1100

    Update sorting module
    Update to use either GridfieldExtensions module OR
    GridFieldOrderableRows module

--- _config.php
+++ _config.php
@@ -1,3 +1,5 @@
 <?php
 
-
+if (!class_exists('GridFieldOrderableRows') && !class_exists('GridFieldSortableRows')) {
+	throw new Exception('The Advanced faq module requires the GridfieldExtensions module OR GridFieldOrderableRows module.');
+}
